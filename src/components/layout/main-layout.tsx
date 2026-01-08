8ae51ab5657bd5c4076f40ee5271e1c03275f79d
Soham Patil <edu.sohampatil@gmail.com>
2026-01-08 12:22:38 +0000

    keep the side bar for the navigation parmanentaly there so that it wont

--- src/components/layout/main-layout.tsx
+++ src/components/layout/main-layout.tsx
@@ -1,21 +1,18 @@
 "use client";
 
-import { SidebarProvider, Sidebar, SidebarInset, SidebarRail } from "@/components/ui/sidebar";
 import { AppSidebar } from "./app-sidebar";
 import { AppHeader } from "./app-header";
 
 export function MainLayout({ children }: { children: React.ReactNode }) {
   return (
-    <SidebarProvider>
-        <div className="flex min-h-screen">
-          <AppSidebar />
-          <div className="flex-1 flex flex-col">
-            <AppHeader />
-            <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
-              {children}
-            </main>
-          </div>
-        </div>
-    </SidebarProvider>
+    <div className="flex min-h-screen w-full">
+      <AppSidebar />
+      <div className="flex flex-1 flex-col sm:pl-14">
+        <AppHeader />
+        <main className="flex-1 p-4 sm:p-6 lg:p-8 bg-background">
+          {children}
+        </main>
+      </div>
+    </div>
   );
 }
